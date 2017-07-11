236aba9005355dd4e5b29e89258e31e7c2b4c32a
Nixon <nixontorres26@gmail.com>
2017-07-11 11:32:46 -0400

    adicion de adminlite en local

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,15 +5,13 @@
         <title>InfyOm Generator</title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/AdminLTE.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/css/skins/_all-skins.min.css">
-
-        <!-- Ionicons -->
-        <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/_all.css">
+        <link href="{{ url('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ url('css/font-awesome.min.css') }}" rel="stylesheet">
+        <link href="{{ url('css/select2.min.css') }}" rel="stylesheet">
+        <link href="{{ url('css/ionicons.min.css') }}" rel="stylesheet">
+        <link href="{{ url('css/AdminLTE.min.css') }}" rel="stylesheet">
+        <link href="{{ url('css/_all-skins.min.css') }}" rel="stylesheet">
+        <link href="{{ url('css/_all.css') }}" rel="stylesheet">
         @yield('css')
     </head>
 
@@ -92,16 +90,13 @@
             </footer>
 
         </div>
-
-
-        <!-- jQuery 3.1.1 -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
+        <script src="{{ url('js/jquery.min.js') }}"></script>
+        <script src="{{ url('js/bootstrap.min.js') }}"></script>
+        <script src="{{ url('js/select2.min.js') }}"></script>
+        <script src="{{ url('js/icheck.min.js') }}"></script>
 
         <!-- AdminLTE App -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.3.11/js/app.min.js"></script>
+        <script src="{{ url('js/app.min.js') }}"></script>
 
         @yield('scripts')
     </body>
