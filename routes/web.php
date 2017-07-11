2816817f4d5a6fa9919be3fd0960dd971b2310e8
Emmanuel Prin <emmanuel_prin@hotmail.com>
2017-07-11 10:27:31 -0400

    configurada la autenticacion

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => 'auth'], function () {
+
+	Route::get('/home', 'HomeController@index');
+});
